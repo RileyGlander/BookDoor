961d6b5c74405e2101178c55c5d4efa00b03cd13
RileyGlander <142702948+RileyGlander@users.noreply.github.com>
2024-07-22 15:24:06 -0500

    changed mongo api again for testing

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/book-door');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://rileyglander:ij9FHlKoRCHKIhau@cluster0.wqqhpdy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 module.exports = mongoose.connection;
